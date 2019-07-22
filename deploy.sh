4f8d1aad72a0df3b7c42392dd6faaa23e0abbd13
Oskar Homburg <oskar@ohomburg.de>
2019-07-22 17:38:25 +0200

    Purge CF cache on build

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@
 rsync -avz --delete-after public/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_DIRECTORY
 
 # Purge the cloudflare cache
-#curl -X POST "https://api.cloudflare.com/client/v4/zones/$CF_ZONE/purge_cache" \
-#    -H "X-Auth-Email: $CF_EMAIL" -H "X-Auth-Key: $CF_AUTH" -H "Content-Type: application/json" \
-#    --data '{"purge_everything":true}'
+curl -X POST "https://api.cloudflare.com/client/v4/zones/$CF_ZONE/purge_cache" \
+    -H "X-Auth-Email: $CF_EMAIL" -H "X-Auth-Key: $CF_AUTH" -H "Content-Type: application/json" \
+    --data '{"purge_everything":true}'
